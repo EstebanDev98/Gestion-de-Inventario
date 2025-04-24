0027af1de3b164ec2bb94718a661f6052fab63e0
velocistacallejero <angelcaracas54@gmail.com>
2025-04-23 22:30:09 -0500

    Borre el modelo insumo y controlador insumo (eran de prueba)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\Insumos\InsumosController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Usuario\UserController;
-use App\Http\Controllers\Insumos\InsumosController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Insumo;
